9dfcc6ddb4657270e451fa14fc3dea936e00941d
dibeher <53269930+dibeher@users.noreply.github.com>
2021-07-13 13:09:29 -0500

    Update Documento.java

--- src/fciencias/edatos/proyecto/Documento.java
+++ src/fciencias/edatos/proyecto/Documento.java
@@ -1,4 +1,25 @@
 public class Documento{
   private String nombre;
-  
+  private String cadena;
+
+  public Documento(String nuevoNombre, String nuevaCadena){
+    this.nombre = nuevoNombre;
+    this.cadena = nuevaCadena;
+  }
+
+  public String getNombre(){
+    return this.nombre;
+  }
+
+  public void setNombre(String nuevoNombre){
+    this.nombre = nuevoNombre;
+  }
+
+  public String getCadena(){
+    return this.nombre;
+  }
+
+  public void setCadena(String nuevaCadena){
+    this.cadena = nuevaCadena;
+  }
 }
